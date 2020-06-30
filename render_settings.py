8016ec6b8524f73edb7bc44b7f2db67f9971aa4b
TrentonJW <twesley@hmc.edu>
2020-06-30 15:40:46 -0500

    Changed vertical offset

--- render_settings.py
+++ render_settings.py
@@ -1,7 +1,7 @@
 # render_settings.py
 import plot_tools
 
-VERTICAL_OFFSET = 0.2      # Offset for drawing parasite nodes above host nodes
+VERTICAL_OFFSET = 0.3      # Offset for drawing parasite nodes above host nodes
 COSPECIATION_OFFSET = .4    # Offest for drawing parasite nodes closer to host 
                             # nodes for speciation events
 NODE_OFFSET = 0.4
